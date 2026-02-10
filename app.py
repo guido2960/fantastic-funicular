8a3f4b493c2ee4535d7bfd64c8753d5158215085
guido2960 <guidoprogramador23@gmail.com>
2026-02-10 02:30:04 -0500

    Update app.py
    Signed-off-by: guido2960 <guidoprogramador23@gmail.com>

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 # CONFIGURACIÓN DE SEGURIDAD PARA MAYDA ❤️
 CODIGO_PUERTA = "amor123"
@@ -62,7 +62,7 @@ def subir():
             
     return redirect(url_for('login'))
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     inicializar_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
