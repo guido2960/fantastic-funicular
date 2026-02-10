b934d17d3c14102d864266b70acb8899324b3e32
guido2960 <guidoprogramador23@gmail.com>
2026-02-10 03:09:44 -0500

    Update app.py
    Signed-off-by: guido2960 <guidoprogramador23@gmail.com>

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sqlite3
 from flask import Flask, render_template, request, redirect, url_for
 
 # Asegúrate de que sean DOS guiones bajos: _name_
-app = Flask(_name_)
+app = Flask(__name__)
 
 # CONFIGURACIÓN DE SEGURIDAD
 CODIGO_PUERTA = "amor123"
@@ -65,7 +65,7 @@ def subir():
             
     return redirect(url_for('login'))
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     inicializar_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
