41ad2152ba5643f3357cbd25fd18edd800eb07c5
A B E  L   23 <guidoprogramador23@gmail.com>
2026-03-07 08:28:59 -0500

    Add access credentials and system configuration
    Signed-off-by: A B E  L   23 <guidoprogramador23@gmail.com>

---
+++ SISTEMA_BOVEDA_PRO/acceso.py
@@ -0,0 +1,11 @@
+# SISTEMA_BOVEDA_PRO/acceso.py
+
+# --- CREDENCIALES ---
+# Estas son tus llaves maestras para entrar al sistema
+USUARIO_ACCESO = "maydaycookingamor@gmail.com" 
+CLAVE_ACCESO = "cariño241125"
+PIN_ADMIN = "2601" 
+
+# --- CONFIGURACIÓN DE NOMBRES ---
+NOMBRE_SISTEMA = "Bóveda Pro"
+VERSION = "1.0.0"
